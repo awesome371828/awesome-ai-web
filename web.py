b3a2931d423eae85d478a87e7979b9c4aedb3031
awesome371828 <sergeylip1904@gmail.com>
2026-08-15 11:46:53 +0300

    Update web.py

--- web.py
+++ web.py
@@ -14,7 +14,7 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 from PIL import Image, ImageEnhance, ImageFilter
 from bs4 import BeautifulSoup
-from dateutil.relativedelta import relativedelta  # ✅ ПРАВИЛЬНО
+from dateutil.relativedelta import relativedelta  # ✅ ПРАВИЛЬНО!
 import sqlite3
 import time
 
